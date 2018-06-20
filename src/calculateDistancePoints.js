556e2f4dd883bff7737916b4353ed6597de68d3d
mnap <learn@10g.pl>
2018-06-20 22:36:07 +0200

    Add calculateDistancePoints function

--- src/calculateDistancePoints.js
+++ src/calculateDistancePoints.js
@@ -1,5 +1,13 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
+    const distancePoints = distance - kPoint;
 
+    if (hillSize < 110) {
+        return distancePoints * 2 + 60;
+    } else if (hillSize >= 110 && hillSize < 185) {
+        return distancePoints * 1.8 + 60;
+    } else {
+        return distancePoints * 1.2 + 120;
+    }
 };
 
 module.exports = calculateDistancePoints;
