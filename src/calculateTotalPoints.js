885fd42df1f182c1da783c9130e0adbd55dab358
mnap <learn@10g.pl>
2018-06-20 22:05:33 +0200

    Add project structure

---
+++ src/calculateTotalPoints.js
@@ -0,0 +1,13 @@
+const calculateDistancePoints = require('./calculateDistancePoints');
+const calculateStylePoints = require('./calculateStylePoints');
+
+const calculateTotalPoints = (
+    distance, hillSize, kPoint, styleNotes, windFactor, gateFactor
+) => {
+    const distancePoints = calculateDistancePoints(distance, hillSize, kPoint);
+    const stylePoints = calculateStylePoints(styleNotes);
+
+    return distancePoints + stylePoints + windFactor + gateFactor;
+};
+
+module.exports = calculateTotalPoints;
