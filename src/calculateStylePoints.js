c2c86fa5c825abb2ecea1ed123d201585c608bb3
mnap <learn@10g.pl>
2018-06-20 22:56:09 +0200

    Add calculateStylePoints function

--- src/calculateStylePoints.js
+++ src/calculateStylePoints.js
@@ -1,5 +1,9 @@
 const calculateStylePoints = (styleNotes) => {
+    const lowestNote = Math.min(...styleNotes);
+    const highestNote = Math.max(...styleNotes);
+    const notesSum = styleNotes.reduce((a, b) => a + b, 0);
 
+    return notesSum - lowestNote - highestNote;
 };
 
 module.exports = calculateStylePoints;
