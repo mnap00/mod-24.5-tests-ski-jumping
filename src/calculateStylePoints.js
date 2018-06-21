f0024b993eaf5453105a0aa335147206939cb7c1
mnap00 <learn@10g.pl>
2018-06-21 14:41:00 +0200

    Fix: import

--- src/calculateStylePoints.js
+++ src/calculateStylePoints.js
@@ -4,15 +4,8 @@ const calculateStylePoints = (styleNotes) => {
     const notesSum = styleNotes.reduce((a, b) => a + b, 0);
 
     const stylePoints = notesSum - lowestNote - highestNote;
-    console.log(isNaN(stylePoints));
-    console.log(isNaN(highestNote));
-    console.log(isNaN(lowestNote));
-    console.log(isNaN(notesSum));
 
     return stylePoints;
 };
-
-var points = calculateStylePoints([17.5, 17.5, 17.5, 17.5, 17.5]);
-console.log(points);
 
 module.exports = calculateStylePoints;
--- src/calculateStylePoints.test.js
+++ src/calculateStylePoints.test.js
@@ -1,9 +1,18 @@
 /*eslint-env mocha*/
 const assert = require('assert');
-const calculateStylePoints = require('./calculateDistancePoints');
+const calculateStylePoints = require('./calculateStylePoints');
 
 describe('stylePoints', () => {
     it('equal notes', () => {
+        const styleNotes = [ 17.5, 17.5, 17.5, 17.5, 17.5 ];
+        const actual = calculateStylePoints(styleNotes);
+
+        const expected = 52.5;
+
+        assert.equal(actual, expected);
+    });
+
+    it('notes', () => {
         const styleNotes = [ 17.5, 17.5, 17.5, 17.5, 17.5 ];
         const actual = calculateStylePoints(styleNotes);
 
