2727c25469fb05be6b0c77fca88038b8251a1ed9
mnap00 <learn@10g.pl>
2018-06-21 16:08:48 +0200

    Add calculateStylePoints test

--- src/calculateStylePoints.test.js
+++ src/calculateStylePoints.test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const calculateStylePoints = require('./calculateStylePoints');
 
 describe('stylePoints', () => {
-    it('equal notes', () => {
+    it('tests equal notes', () => {
         const styleNotes = [ 17.5, 17.5, 17.5, 17.5, 17.5 ];
         const actual = calculateStylePoints(styleNotes);
 
@@ -12,11 +12,20 @@ describe('stylePoints', () => {
         assert.equal(actual, expected);
     });
 
-    it('notes', () => {
-        const styleNotes = [ 17.5, 17.5, 17.5, 17.5, 17.5 ];
+    it('tests border notes', () => {
+        const styleNotes = [ 0.0, 5.5, 11.0, 17.5, 20.0 ];
         const actual = calculateStylePoints(styleNotes);
 
-        const expected = 52.5;
+        const expected = 34;
+
+        assert.equal(actual, expected);
+    });
+
+    it('tests balanced notes', () => {
+        const styleNotes = [ 16.5, 17.0, 16.0, 16.5, 17.0 ];
+        const actual = calculateStylePoints(styleNotes);
+
+        const expected = 50;
 
         assert.equal(actual, expected);
     });
